0f11149f350a91d0f4cdb01ce3e82467610e1658
Yusra Ahmed <yusra.ahmed05@gmail.com>
2017-10-22 04:09:48 -0700

    added animation to story and bridal party

--- resources/js/script.js
+++ resources/js/script.js
@@ -49,4 +49,17 @@ $(document).ready(function() {
         }
       });
     
+    // Animations on scroll
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated slideInRight');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
 });
